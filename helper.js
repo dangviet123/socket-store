5e15a545c6a4c363b3930d41f9dff9056b7adb00
viet <dangquocviet070394@gmail.com>
2022-03-25 17:46:19 +0700

    x

--- helper.js
+++ helper.js
@@ -7,19 +7,54 @@ const pushSocketIdToArray = (clients, userId, socketId) => {
     return clients;
 }
 
+const pushSocketIdToArrayInfo = (clients, userId, socketId, infos) => {
+  if (userId > 0) {
+    if(clients[userId]){
+        clients[userId].push({
+          socketId: socketId,
+          date: new Date(),
+          full_name: infos.query.full_name,
+          headers: infos.headers
+        });
+    }else{
+        clients[userId] = [
+          {
+            socketId: socketId,
+            date: new Date(),
+            full_name: infos.query.full_name,
+            headers: infos.headers
+          }
+        ];
+    }
+    return clients;
+  }
+}
+
+const removeSocketIdToArrayInfo = (clients, userId, socket, headers)=>{
+  if (userId > 0) {
+    clients[userId] = clients[userId].filter(item=> item.socketId !== socket.id);
+    if(!clients[userId].length){
+        delete clients[userId];
+    }
+    return clients;
+  }
+}
+
 const removeSocketIdToArray = (clients, userId, socket)=>{
+  if (userId > 0) {
     clients[userId] = clients[userId].filter(socketId=>socketId !== socket.id);
     if(!clients[userId].length){
         delete clients[userId];
     }
     return clients;
+  }
 }
 
-const emitNotifyToArray = (clients, userId, socket, eventName, data) => {
+const emitNotifyToArray = (clients, userId, io, eventName, data) => {
   if (clients[userId]) {
-    return clients[userId].forEach(socketId =>
-      socket.to(socketId).emit(eventName, data)
-    );
+    return clients[userId].forEach(socketId => {
+      io.to(socketId).emit(eventName, data);
+    });
   } 
 };
 
@@ -33,7 +68,9 @@ const emitNotifyToArrayNameSpace = (clients, userId, namspace, eventName, data) 
 
 module.exports = {
   pushSocketIdToArray,
+  pushSocketIdToArrayInfo,
   removeSocketIdToArray,
   emitNotifyToArray,
-  emitNotifyToArrayNameSpace
+  emitNotifyToArrayNameSpace,
+  removeSocketIdToArrayInfo
 };
--- server.js
+++ server.js
@@ -9,11 +9,14 @@ var jwt = require('jsonwebtoken');
 const io = sockecio(server, {
     cors: {
       origin: '*',
+      //methods: ["GET", "POST"]
     }
 });
 const {
     pushSocketIdToArray,
     emitNotifyToArray,
+    pushSocketIdToArrayInfo,
+    removeSocketIdToArrayInfo,
     removeSocketIdToArray,
 } = require("./helper");
 
@@ -22,31 +25,45 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 let clients = {};
+let clientsInfo = {};
 app.use(cors());
 io.on('connection', (socket) => {
-    
+    //console.log("New Connection with transport", socket.conn.transport.name);
     try {   
+
         let id = socket.handshake.query.id;
+        //.log(socket.handshake.headers);
         if (id) {
             clients = pushSocketIdToArray(clients, id, socket.id);
-            //console.log(clients);
+            clientsInfo = pushSocketIdToArrayInfo(clientsInfo, id, socket.id, socket.handshake);
         }
 
         // tất cả user online khi người dùng đăng nhập
         socket.emit('all-user-online', Object.keys(clients));
-        socket.broadcast.emit('user-login', Object.keys(clients));
+
+        socket.broadcast.emit('user-login', clientsInfo);
+
+
+
+        socket.on('send-user-online', (userId) => {
+            emitNotifyToArray(clients, userId, io, 'reciver-user-online', clientsInfo);
+            //socket.emit('reciver-user-online', Object.keys(clients));
+        })
+
+
 
         // user đóng kết nối
         socket.on("disconnect", () => {
             clients = removeSocketIdToArray(clients, id, socket);
-            socket.broadcast.emit('user-logout', Object.keys(clients));
-
-            console.log('người dùng đóng kết nối');
+            clientsInfo = removeSocketIdToArrayInfo(clientsInfo, id, socket, socket.handshake);
+            socket.broadcast.emit('user-logout', clientsInfo);
 
         });
     } catch (error) {
         console.log(error);
     }
+
+
 
     // test gửi data
     socket.on('chat message', (msg) => {
