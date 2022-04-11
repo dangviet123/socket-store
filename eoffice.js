2dcbc072ea6ccb88db20cdfe85d470c30a3c4512
viet <dangquocviet070394@gmail.com>
2022-04-11 23:34:14 +0700

    c

--- eoffice.js
+++ eoffice.js
@@ -6,6 +6,8 @@ const {
     removeSocketIdToArray,
 } = require("./helper");
 
+const apis = require('./apiConfig');
+
 const userOnlineEoffice = (io) => {
     let clients = {};
     let clientsInfo = {};
@@ -17,21 +19,40 @@ const userOnlineEoffice = (io) => {
                 clientsInfo = pushSocketIdToArrayInfo(clientsInfo, id, socket.id, socket.handshake);
             }
             // tất cả user online khi người dùng đăng nhập
-            socket.emit('all-user-online', Object.keys(clients));
+            //socket.emit('all-user-online', Object.keys(clients));
     
             socket.broadcast.emit('user-login', clientsInfo);
-    
+            
+
+
             socket.on('send-user-online', (userId) => {
                 emitNotifyToArray(clients, userId, io, 'reciver-user-online', clientsInfo);
                 //socket.emit('reciver-user-online', Object.keys(clients));
+            });
+
+
+            socket.on('send-logout-request', (data) => {
+                if (data.length > 0) {
+                    data.forEach(element => {
+                        emitNotifyToArray(clients, element, io, 'reciver-user-online', {logout: true});
+                    });
+                    
+                }
             })
+
+
     
             // user đóng kết nối
             socket.on("disconnect", () => {
                 socket.disconnect();
                 clients = removeSocketIdToArray(clients, id, socket);
                 clientsInfo = removeSocketIdToArrayInfo(clientsInfo, id, socket, socket.handshake);
+
                 socket.broadcast.emit('user-logout', clientsInfo);
+
+                if (!clients[id] && id !== 0) {
+                    apis.post('auth/user-offline', {id_user: id});
+                }
     
             });
         } catch (error) {
